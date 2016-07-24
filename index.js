6914912e55a70bf795be031003d514f8b16ef12a
Steven R Terpe <srterpe@gmail.com>
2016-07-24 01:38:48 -0700

    Add package.json, index.js, .gitignore

---
+++ index.js
@@ -0,0 +1,57 @@
+var xml2js = require('xml2js')
+var parseMs = require('parse-ms')
+var leftPad = require('left-pad')
+
+module.exports = function (timedTextXML, cb) {
+  xml2js.parseString(timedTextXML, function (err, result) {
+    if (err) {
+      return cb(err)
+    }
+    var timedtext = result.timedtext
+    switch (timedtext.$.format) {
+      case '3':
+        parseFormat3(timedtext.body[0].p, cb)
+        break
+      default:
+        cb(new Error([
+          'Unknown timedtext format',
+          '`' + timedText.$.format + '`.'
+        ].join(' ')))
+    }
+  })
+}
+function parseFormat3(p, cb) {
+  var s = ''
+  try {
+    for (var i = 0; i < p.length; ++i) {
+      var t = parseInt(p[i].$.t, 10)
+      var d = parseInt(p[i].$.d, 10)
+      d = parseMs(t + d)
+      t = parseMs(t)
+      var startTime = [
+        leftPad(t.hours, 2, 0),
+        leftPad(t.minutes, 2, 0),
+        leftPad(t.seconds, 2, 0)
+      ].join(':') + ',' + t.milliseconds
+      var endTime = [
+        leftPad(d.hours, 2, 0),
+        leftPad(d.minutes, 2, 0),
+        leftPad(d.seconds, 2, 0)
+      ].join(':') + ',' + d.milliseconds
+      var subtitle = [
+        i,
+        [
+          startTime,
+          '-->',
+          endTime
+        ].join(' '),
+        p[i]._,
+        '\n'
+      ].join('\n')
+      s += subtitle
+    }
+  } catch (e) {
+    return cb(e)
+  }
+  cb(null, s)
+}
