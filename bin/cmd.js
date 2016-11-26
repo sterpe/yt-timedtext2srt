c757c3eb3a425d3d0e6a1fd718691741bd4a5d86
Steven R Terpe <srterpe@gmail.com>
2016-11-25 18:41:20 -0800

    Fix lint errors in bin/cmd.js

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,22 +2,23 @@
 var data
 if (process.stdin.isTTY) {
 } else {
-	data = ''
-	process.stdin.setEncoding('utf-8')
-	process.stdin.on('readable', function () {
-		var chunk
-		while (chunk = process.stdin.read()) {
-			data += chunk
-		}
-	})
-	process.stdin.on('end', function () {
-		data = data.replace(/\n$/, '')
-		require('../')(data, function (err, result) {
-			if (err) {
-				throw err
-			} else {
-				process.stdout.write(result)
-			}
-		})
-	})
+  data = ''
+  process.stdin.setEncoding('utf-8')
+  process.stdin.on('readable', function () {
+    var chunk = process.stdin.read()
+    while (chunk) {
+      data += chunk
+      chunk = process.stdin.read()
+    }
+  })
+  process.stdin.on('end', function () {
+    data = data.replace(/\n$/, '')
+    require('../')(data, function (err, result) {
+      if (err) {
+        throw err
+      } else {
+        process.stdout.write(result)
+      }
+    })
+  })
 }
